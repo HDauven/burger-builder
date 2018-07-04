fd0aa78330c56d35100134e7d9aede756d4ca773
Hein Dauven <heindauven@gmail.com>
2018-07-04 16:21:05 +0200

    Only show invalid for form inputs that have a validation object.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -154,6 +154,8 @@ class ContactData extends Component {
             elementConfig={formElement.config.elementConfig}
             elementType={formElement.config.elementType} 
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
         <Button btnType="Success">ORDER</Button>
